6980d2f83853e7cecfccbe02c457cf1e2c823752
Xiao Nan <road2stat@gmail.com>
2015-01-13 21:49:36 +0800

    add comments for snp-mapping script

--- SNP-mapping.R
+++ SNP-mapping.R
@@ -1,32 +1,46 @@
 setwd('~/Desktop/')
 
+## load gene lists to be mapped for their snps
 sheet1 = read.csv('sheet1-aging.csv', as.is = TRUE)
 sheet2 = read.csv('sheet2-longevity.csv', as.is = TRUE)
 sheet3 = read.csv('sheet3-longevity.csv', as.is = TRUE)
 
+## load snp-gene mapping table
 mapping = read.csv('IBC_chip_final_49K SNP_list_BroadInst.csv', as.is = TRUE)
 
+## get uniqued gene symbol vector from sheet 1
 gene1 = unique(sheet1$HGNC.Symbol)
 
-# TODO: deal with the 'mutiple' ones
+## get uniqued gene symbol vector from sheet 2
+## TODO: deal with the 'mutiple' ones
 gene2 = unique(sheet2$Gene.Variant)[-which(unique(sheet2$Gene.Variant) == '')]
 
+## get uniqued gene symbol vector from sheet 3
 gene3 = unique(unlist(strsplit(sheet3$gene.s., ',')))
 
-##
+## create a list named genemap to store the known snp-gene mapping
+## each snp in the data frame mapping may have several corresponding genes
+## each component of the list stores a gene symbol vector for the snp
 genemap = vector('list', nrow(mapping))
+## split (and remove) the original gene symbol vector for comma
 for (i in 1L:length(genemap)) genemap[[i]] = unlist(strsplit(mapping$RefSeq_hg18_Gene[i], ','))
+## split (and remove) the original gene symbol vector for semicolon
 for (i in 1L:length(genemap)) genemap[[i]] = unlist(strsplit(genemap[[i]], ';'))
-for (i in 1L:length(genemap)) genemap[[i]] = unlist(strsplit(genemap[[i]], ';'))
+## replace "NEAR GENE:" to "" for the original gene symbol vector
 for (i in 1L:length(genemap)) genemap[[i]] = gsub('NEAR GENE:', '', genemap[[i]])
+## replace "IN GENE:" to "" for the original gene symbol vector
 for (i in 1L:length(genemap)) genemap[[i]] = gsub('IN GENE:', '', genemap[[i]])
+## get the uniqued gene symbols for each snp
 for (i in 1L:length(genemap)) genemap[[i]] = unique(genemap[[i]])
 
-##
+## get the uniqued gene symbol vector from sheet 1
 candi_genes1 = unique(gene1)
+## create a list named candi_snps1 to store the gene-snp mapping to be made
 candi_snps1 = vector('list', length(candi_genes1))
+## name each component of the list by the gene symbol
 names(candi_snps1) = candi_genes1
 
+## looking for the corresponding snps for each gene
 for (i in 1L:length(candi_genes1)) {
   print(i)
   for (j in 1L:length(genemap)) {
@@ -35,13 +49,17 @@ for (i in 1L:length(candi_genes1)) {
   }
 }
 
+## get the uniqued snp vector for sheet 1
 candi_snps1 = unique(unlist(candi_snps1))
 
-##
+## get the uniqued gene symbol vector from sheet 2
 candi_genes2 = unique(gene2)
+## create a list named candi_snps2 to store the gene-snp mapping to be made
 candi_snps2 = vector('list', length(candi_genes2))
+## name each component of the list by the gene symbol
 names(candi_snps2) = candi_genes2
 
+## looking for the corresponding snps for each gene
 for (i in 1L:length(candi_genes2)) {
   print(i)
   for (j in 1L:length(genemap)) {
@@ -50,13 +68,17 @@ for (i in 1L:length(candi_genes2)) {
   }
 }
 
+## get the uniqued snp vector for sheet 2
 candi_snps2 = unique(unlist(candi_snps2))
 
-##
+## get the uniqued gene symbol vector from sheet 3
 candi_genes3 = unique(gene3)
+## create a list named candi_snps3 to store the gene-snp mapping to be made
 candi_snps3 = vector('list', length(candi_genes3))
+## name each component of the list by the gene symbol
 names(candi_snps3) = candi_genes3
 
+## looking for the corresponding snps for each gene
 for (i in 1L:length(candi_genes3)) {
   print(i)
   for (j in 1L:length(genemap)) {
@@ -65,13 +87,17 @@ for (i in 1L:length(candi_genes3)) {
   }
 }
 
+## get the uniqued snp vector for sheet 3
 candi_snps3 = unique(unlist(candi_snps3))
 
-##
+## get the uniqued gene symbol vector after combining sheet 1, 2, and 3
 candi_genes = unique(c(gene1, gene2, gene3))
+## create a list named candi_snps to store the gene-snp mapping to be made
 candi_snps = vector('list', length(candi_genes))
+## name each component of the list by the gene symbol
 names(candi_snps) = candi_genes
 
+## looking for the corresponding snps for each gene
 for (i in 1L:length(candi_genes)) {
   print(i)
   for (j in 1L:length(genemap)) {
@@ -80,8 +106,10 @@ for (i in 1L:length(candi_genes)) {
   }
 }
 
+## get the uniqued snp vector for all sheets
 candi_snps = unique(unlist(candi_snps))
 
+## write candidate snps, without gene names
 write(candi_snps1, file = 'candi-snps-1.csv')
 write(candi_snps2, file = 'candi-snps-2.csv')
 write(candi_snps3, file = 'candi-snps-3.csv')
