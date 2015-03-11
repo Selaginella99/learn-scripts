0459ebfefa2dfc86d8e36329ecaec0c2a792900a
teacheRforV <31927761@qq.com>
2015-03-11 13:06:00 -0400

    Replace NA with SNP ID

--- overlap-snp-gene-mapping.R
+++ overlap-snp-gene-mapping.R
@@ -20,5 +20,10 @@ rs_intersect = intersect(marker_rs_f, marker_rs_m)  # get intersect set of M & F
 ls_final_table = ibctable[ls_intersect, c('SNP_ID', 'Chromosome', 'RefSeq_hg18_Gene')]
 rs_final_table = ibctable[rs_intersect, c('SNP_ID', 'Chromosome', 'RefSeq_hg18_Gene')]
 
+ls_na_id = which(is.na(ls_final_table$SNP_ID))  
+rs_na_id = which(is.na(rs_final_table$SNP_ID))  
+ls_final_table[ls_na_id, 'SNP_ID'] = ls_intersect[ls_na_id]  # refill snp id to the NA rows of the snp id column
+rs_final_table[rs_na_id, 'SNP_ID'] = rs_intersect[rs_na_id]  # refill snp id to the NA rows of the snp id column
+
 write.csv(ls_final_table, file = 'intersect_table_ls.csv', row.names = FALSE, quote = FALSE)
 write.csv(rs_final_table, file = 'intersect_table_rs.csv', row.names = FALSE, quote = FALSE)
