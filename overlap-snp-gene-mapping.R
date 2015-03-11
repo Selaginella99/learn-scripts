367b57d8f4daef8cbb0c42a0e46c375372c5a5fb
teacheRforV <31927761@qq.com>
2015-03-11 12:20:23 -0400

    overlap snps to genes

---
+++ overlap-snp-gene-mapping.R
@@ -0,0 +1,24 @@
+## Find the overlap of SNPs between male & female for LS & RS and map back to genes
+## Date: March 11, 2015
+
+snp_ls_f = read.csv('Markers LS F_chi.csv', header = TRUE, as.is = TRUE)  # load snp of lifespan table of female (continuous outcome)
+snp_ls_m = read.csv('Markers LS M_chi.csv', header = TRUE, as.is = TRUE)  # load snp of lifespan table of male (continuous outcome)
+snp_rs_f = read.csv('Markers RS F_chi.csv', header = TRUE, as.is = TRUE)  # load snp table of female (continuous outcome)
+snp_rs_m = read.csv('Markers RS M_chi.csv', header = TRUE, as.is = TRUE)  # load snp table of male (continuous outcome)
+
+ibctable = read.csv('IBC_chip_final_49K SNP_list_BroadInst.csv', header = TRUE, as.is = TRUE)
+row.names(ibctable) = ibctable$SNP_ID
+
+marker_ls_f = snp_ls_f$Marker  # extract marker from table: ls female snp id (character vector)
+marker_ls_m = snp_ls_m$Marker  # extract marker from table: ls male snp id (character vector)
+marker_rs_f = snp_rs_f$Marker  # extract marker from table: rs female snp id (character vector)
+marker_rs_m = snp_rs_m$Marker  # extract marker from table: rs male snp id (character vector)
+
+ls_intersect = intersect(marker_ls_f, marker_ls_m)  # get intersect set of M & F for LS
+rs_intersect = intersect(marker_rs_f, marker_rs_m)  # get intersect set of M & F for RS
+
+ls_final_table = ibctable[ls_intersect, c('SNP_ID', 'Chromosome', 'RefSeq_hg18_Gene')]
+rs_final_table = ibctable[rs_intersect, c('SNP_ID', 'Chromosome', 'RefSeq_hg18_Gene')]
+
+write.table(ls_final_table, file = 'intersect_table_ls.tsv', row.names = FALSE, quote = FALSE, sep = '\t')
+write.table(rs_final_table, file = 'intersect_table_rs.tsv', row.names = FALSE, quote = FALSE, sep = '\t')
