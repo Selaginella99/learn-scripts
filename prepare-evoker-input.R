d7487baa9ff3d2fb29024f3bf3e85ea9734e0896
teacheRforV <31927761@qq.com>
2015-03-12 12:12:25 -0400

    prepare Evoker input data to generate .bnt files

---
+++ prepare-evoker-input.R
@@ -0,0 +1,23 @@
+genoprefix = 'phg000004.ind.geno.'  # set the prefix of the genotype files here
+
+famfile = list.files()[grep('.fam', list.files())]
+famtable = read.table(famfile, sep = ' ', header = FALSE, as.is = TRUE)
+indindex = as.character(famtable$V2)
+
+snps = read.table(paste0(genoprefix, indindex[1L]), sep = ',', header = FALSE, as.is = TRUE)[, 1L]
+
+evoker_df = as.data.frame(matrix(NA, nrow = length(snps), ncol = (2L * length(indindex)) + 1L))
+names(evoker_df) = c('SNP', rep(indindex, each = 2L))
+evoker_df$SNP = snps
+
+for (i in 1L:length(indindex)) {
+  cat('Merging', i, 'in', length(indindex), '\n')
+  tmp = read.table(paste0(genoprefix, indindex[i]), 
+                   sep = ',', header = FALSE, 
+                   colClasses = c(rep('NULL', 8L), 
+                                  rep('numeric', 2)))  # load faster
+  evoker_df[, 2L * i] = tmp[, 1L]
+  evoker_df[, 2L * i + 1L] = tmp[, 2L]
+}
+
+write.table(evoker_df, file = 'input.txt', sep = ' ', quote = FALSE, col.names = TRUE, row.names = FALSE)
