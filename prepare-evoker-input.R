d66b22781ea12fbc36763f95ffcdd4f206274d47
Selaginella_V <veronica9907@sina.com>
2015-03-15 10:48:22 -0400

    More Efficient Version of Evoker Input Preparation

--- prepare-evoker-input.R
+++ prepare-evoker-input.R
@@ -1,8 +1,9 @@
 genoprefix = 'phg000004.ind.geno.'  # set the prefix of the genotype files here
 
 famfile = list.files()[grep('.fam', list.files())]
-famtable = read.table(famfile, sep = ' ', header = FALSE, as.is = TRUE)
+famtable = read.table(famfile[1], sep = ' ', header = FALSE, as.is = TRUE)
 indindex = as.character(famtable$V2)
+# indindex = c('5', '9', '26', '33', '35')  # for testing
 
 snps = read.table(paste0(paste0(genoprefix, indindex[1L]), '.gz'), sep = ',', header = FALSE, as.is = TRUE)[, 1L]
 
@@ -11,13 +12,14 @@ names(evoker_df) = c('SNP', rep(indindex, each = 2L))
 evoker_df$SNP = snps
 
 for (i in 1L:length(indindex)) {
-  cat('Merging', i, 'in', length(indindex), '\n')
-  tmp = read.table(paste0(paste0(genoprefix, indindex[i]), '.gz'),
-                   sep = ',', header = FALSE, 
-                   colClasses = c(rep('NULL', 8L), 
-                                  rep('numeric', 2)))  # load faster
-  evoker_df[, 2L * i] = tmp[, 1L]
-  evoker_df[, 2L * i + 1L] = tmp[, 2L]
-}
+   cat('Loading', i, 'in', length(indindex), '\n')
+   eval(parse(text = paste0('tmp_', i, ' = read.table(paste0(paste0(genoprefix, indindex[', i, ']), ".gz"), 
+   sep = ",", header = FALSE, colClasses = c(rep("NULL", 8L), rep("character", 2L)))')))
+ }
+ 
+gc()
+evoker_mat = do.call(cbind, lapply(paste0('tmp_', 1L:length(indindex)), get))
+evoker_mat = cbind(snps, evoker_mat)
+colnames(evoker_mat) = c('SNP', rep(indindex, each = 2L))
 
-write.table(evoker_df, file = 'input.txt', sep = ' ', quote = FALSE, col.names = TRUE, row.names = FALSE)
+write.table(evoker_mat, file = 'input.txt', sep = ' ', quote = FALSE, col.names = TRUE, row.names = FALSE)
